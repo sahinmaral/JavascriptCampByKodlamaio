e4d0a81ff8cbc253b13920bde31a613a2b81c870
sahin.maral <sahin.maral@hotmail.com>
2021-10-19 00:25:38 +0300

    19th November Lecture Done

---
+++ javaScriptStarterKit/src/app.js
@@ -0,0 +1,11 @@
+console.log("Merhaba kodlama.io");
+
+let krediler = ["Konut Kredileri" , "Emlak Konut Kredileri","Taşıt Kredileri"];
+
+console.log("<ul>")
+
+for (let i = 0; i<krediler.length; i++){
+    console.log("<li>"+krediler[i]+"</li>")
+}
+
+console.log("</ul>")
